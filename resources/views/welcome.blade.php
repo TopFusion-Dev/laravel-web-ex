160e0ddd8434df8028539410d926fd7958d1dbbb
TopFusion.Dev <TopFusion.Dev@gmail.com>
2020-12-29 10:08:47 +0700

    [test]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div>
-                    <h1>Hello Word!</h1>
+                    <h1>Hello Word! Test</h1>
                 </div>
 
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
